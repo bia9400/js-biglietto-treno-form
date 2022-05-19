b25963ab12d04e68b8da383e6843ae13d4ca0d2e
bia9400 <ferro.biagio@gmail.com>
2022-05-19 16:42:21 +0200

    bugfix

--- app.js
+++ app.js
@@ -1,65 +1,67 @@
-let buttonGenerate=document.getElementById("button1")
-let deleteButton=document.getElementById("button2");
-deleteButton.addEventListener("click", function(){
+let buttonGenerate = document.getElementById("button1")
+let deleteButton = document.getElementById("button2");
+deleteButton.addEventListener("click", function () {
     window.location.reload();
 });
 buttonGenerate.addEventListener("click", function () {
-    
+
 
     let name = document.getElementById("userName");
-    
+
     let eta = document.getElementById("userEtà");
     let km = document.getElementById("userKm");
-   
+
     let tariffaAlKilometro = km.value * 0.21;
     let myName = name.value;
     let myEta = eta.value;
     let myKm = km.value;
-    
-    
- 
 
-        if (myEta <= 0 || myKm <= 0) {
-            alert("Wow non sei ancora nato? O sai viaggiare per distanze negative o nulle? inserisci dei valori positivi e maggiori di 0 nei campi richiesti")
+
+
+    if (myName <= 0 || myKm <= 0) {
+        alert("Inserisci il tuo nome e cognome")
+    }
+    else if (myEta <= 0 || myKm <= 0) {
+        alert("Wow non sei ancora nato? O sai viaggiare per distanze negative o nulle? inserisci dei valori positivi e maggiori di 0 nei campi richiesti")
+    }
+    else if (myKm > 0 && myKm < 10) {
+        alert("La tua tratta non esiste, inserire un chilometraggio più grande")
+    }
+    else if (myKm > 2000) {
+        alert("Seee vabbéé siamo Trenitalia mica Starlink..Inserisci un chilometraggio più corto")
+    }
+    else if (myEta > 0 && myEta < 5) {
+        alert("Il biglietto per i minori di 6 anni è cmpletamente gratuito")
+
+    }
+    else {
+        let showTicket = document.querySelector(".ticket-generator")
+        showTicket.className = "d-block";
+        let userNamePrint = document.querySelector(".nameChanged");
+        userNamePrint.innerHTML = `${myName}`;
+        console.log("userNamePrint ", userNamePrint, "myName", myName)
+        if (myEta < 18) {
+            prezzoBiglietto = tariffaAlKilometro - (tariffaAlKilometro * 0.2)
+            let printTicket = document.querySelector(".costoBiglietto")
+
+            printTicket.innerHTML = (` ${(Math.round(prezzoBiglietto * 100) / 100)} `)
         }
-        else if (myKm > 0 && myKm < 10) {
-            alert("La tua tratta non esiste, inserire un chilometraggio più grande")
-        }
-        else if (myKm > 2000) {
-            alert("Seee vabbéé siamo Trenitalia mica Starlink..Inserisci un chilometraggio più corto")
-        }
-        else if (myEta > 0 && myEta < 5) {
-            alert("Il biglietto per i minori di 6 anni è cmpletamente gratuito")
-           
+        else if (myEta > 65) {
+            prezzoBiglietto = tariffaAlKilometro - (tariffaAlKilometro * 0.4)
+            let printTicket = document.querySelector(".costoBiglietto")
+
+            printTicket.innerHTML = (` ${(Math.round(prezzoBiglietto * 100) / 100)} `)
         }
         else {
-            let showTicket=document.querySelector(".ticket-generator")
-            showTicket.className="d-block";
-            let userNamePrint = document.querySelector(".nameChanged");
-            userNamePrint.innerHTML= `${myName}` ;
-            console.log("userNamePrint ",userNamePrint, "myName", myName)
-            if (myEta < 18) {
-                prezzoBiglietto = tariffaAlKilometro - (tariffaAlKilometro * 0.2)
-                let printTicket = document.querySelector(".costoBiglietto")
+            prezzoBiglietto = tariffaAlKilometro
+            let printTicket = document.querySelector(".costoBiglietto")
 
-                printTicket.innerHTML = (` ${(Math.round(prezzoBiglietto * 100) / 100)} `)
-            }
-            else if (myEta > 65) {
-                prezzoBiglietto = tariffaAlKilometro - (tariffaAlKilometro * 0.4)
-                let printTicket = document.querySelector(".costoBiglietto")
+            printTicket.innerHTML = (` ${(Math.round(prezzoBiglietto * 100) / 100)} `)
 
-                printTicket.innerHTML = (` ${(Math.round(prezzoBiglietto * 100) / 100)} `)
-            }
-            else {
-                prezzoBiglietto = tariffaAlKilometro
-                let printTicket = document.querySelector(".costoBiglietto")
-
-                printTicket.innerHTML = (` ${(Math.round(prezzoBiglietto * 100) / 100)} `)
-
-            }
         }
+    }
 
-    }) 
+})
 
 /* functione () {
     let userName = document.getElementById("floatingName");
